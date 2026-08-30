2ee1e2734e511d1529651d4b473c26433a7d6653
Aleksandra Ristovska <aleksandra@ristovska.com>
2026-08-30 08:05:30 +0200

    Create Quiz component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,15 @@ import Information from "@/components/Information";
 export default function Home() {
   const startQuiz = () => {
     console.log("Quiz started")
-  }
+  } 
 
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <main className="flex-1">
-        <Information startQuiz={startQuiz} />
+        <Information startQuiz={startQuiz} /> 
       </main>
       <Footer />
     </div>
-  );
+  )
 }
---
+++ src/components/Quiz/index.tsx
@@ -0,0 +1,76 @@
+import { Question } from "@/types/questions";
+
+type quizProps = {
+    handleAnswerSelect: (answer: string) => void
+    handleNextQuestion: () => void
+    score: number
+    totalQuestions: number
+    currentQuestion: Question
+    currentQuestionIndex: number
+    selectedAnswer: string | null
+}
+
+const Quiz = ({
+    handleAnswerSelect,
+    handleNextQuestion,
+    score,
+    totalQuestions,
+    currentQuestion,
+    currentQuestionIndex,
+    selectedAnswer
+}: quizProps) => {
+
+    return (
+        <section className="px-4 py-10 sm:px-6">
+            <div className="mx-auto max-w-2xl">
+                <img
+                    src={currentQuestion.imageUrl}
+                    alt="Quiz question"
+                    className="w-full rounded-xl"
+                />
+                
+                <div className="mt-4 flex justify-between">
+                    <p> Question {currentQuestionIndex + 1} of {totalQuestions}</p>
+                    <p>Score: {score}</p>
+                </div>
+
+                <div className="mt-6 flex flex-col gap-3">
+                    {currentQuestion.options.map((option, index) => {
+                        let buttonStyle =
+                            "rounded border p-3 text-left hover:cursor-pointer"
+
+                        if (
+                            selectedAnswer === option &&
+                            option === currentQuestion.correctAnswer
+                        ) {
+                            buttonStyle =
+                                "rounded border p-3 text-left text-green-800"
+                        }
+
+                        return (
+                            <button
+                                key={index}
+                                data-testid="quiz-button"
+                                onClick={() => handleAnswerSelect(option)}
+                                className={buttonStyle}
+                            >
+                                {option}
+                            </button>
+                        )
+                    })}
+                </div>
+                {selectedAnswer && (
+                    <button
+                        data-testid="next-question"
+                        onClick={handleNextQuestion}
+                        className="mt-6 rounded bg-amber-400 px-6 py-3 font-semibold hover:cursor-pointer hover:bg-amber-600"
+                    >
+                        Next question
+                    </button>
+                )}
+            </div>
+        </section>
+    )
+}
+
+export default Quiz
